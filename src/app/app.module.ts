961bcbbfdcd27d155b21285abf3e5641faea900b
Unai-Gomez-Diez <unaigomez0807@gmail.com>
2023-12-20 17:13:55 +0100

    ejercicio terminado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MessagesComponent,
+
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HeroesComponent,
+    HeroDetailComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
